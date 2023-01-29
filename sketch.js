87e52daa28ade76661c79ea5bc0d6c048e5ec303
conradbooker <conrad.booker@gmail.com>
2023-01-29 13:20:02 -0500

    stashing

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ let honorCodeImage
 let roomArr=[];
 
 function preload() {
-    roomArr[5] = loadImage("files/101.png")
+    roomArr[4] = loadImage("files/101.png")
     roomArr[2] = loadImage("files/classroom.png")
     roomArr[1] = loadImage("files/libery.png")
-    roomArr[4] = loadImage("files/lockers.png")
+    roomArr[3] = loadImage("files/lockers.png")
     roomArr[0] = loadImage("files/turf.png")
-    roomArr[3] = loadImage("files/honorCodeImage.png")
+    honorCodeImage = loadImage("files/honorCodeImage.png")
 }
 function setup(){
     createCanvas(650,500)

f884b827a041415339d5ee3bd05bc280ccea2f26
conradbooker <conrad.booker@gmail.com>
2023-01-29 12:30:58 -0500

    new photos and things

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+let exitImage
+let classroomImage
+let libraryImage
+let lockersImage
+let turfImage
+let honorCodeImage
+
+function preload() {
+    exitImage = loadImage("files/101.png")
+    classroomImage = loadImage("files/classroom.png")
+    libraryImage = loadImage("files/libery.png")
+    lockersImage = loadImage("files/lockers.png")
+    turfImage = loadImage("files/turf.png")
+    honorCodeImage = loadImage("files/honorCodeImage.png")
+    exitImage = loadImage("files/101.png")
+}

7c0d028a90d711167b1736ba6463971aa5edad2a
conradbooker <conrad.booker@gmail.com>
2023-01-29 12:21:40 -0500

    hiiii

---
+++ objects/objects.js
@@ -0,0 +1,63 @@
+class pen {
+    constructor() {
+          
+      this.x = random(10,width - 10);
+      this.y = random(10,80);
+      
+      this.speed = 1;
+      
+      this.weight = 10;
+      
+      this.type = type;
+      
+    }
+    
+    show() {
+      
+      if (this.type === "food") {
+        
+        fill(227, 64, 64)
+        stroke(255)
+        circle(this.x, this.y, this.weight);
+  
+      }
+      else if (this.type === "poison") {
+        
+        fill(50, 184, 85)
+        stroke(255)
+        circle(this.x, this.y, this.weight);
+  
+      }
+      
+    }
+      
+    update() {
+      
+      this.show();
+      this.move()
+    
+    }
+    
+    move() {
+      
+      this.y += this.speed;
+      
+      if (this.y > 600) this.speed = 0;
+      
+      
+    }
+    
+  }
+  
+class lockerCode {
+
+}
+
+class honorCode {
+    constructor(hasPen) {
+        this.hasPen = hasPen
+        this.signature = ""
+        this
+
+    }
+}
